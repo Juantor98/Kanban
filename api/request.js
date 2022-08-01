a7293830323e413ba374c71907a90b969af2c0ba
Juan David Torres <juantor98@gmail.com>
2022-07-31 20:40:11 -0500

    Organizar request.js
    Eliminación de código que no es necesario

--- api/request.js
+++ api/request.js
@@ -18,31 +18,4 @@ const importData = () => {
   });
 };
 
-
-
-// const showAllTasks = (data) => {
-//     data.map((task) => addNewTask(task))
-// }
-
-
-// const addNewTask = (task) => {
-//     document.getElementById("todoTasks").insertAdjacentHTML('beforeend',
-//         '<article class="notification" style="background-color: rgba(36, 156, 204, 0.863);">'+
-//             '<h3 class="has-text-left" style="height: 20px;">'+ '<strong>Tarea: </strong>'+ task.task.value + '</h3>'+
-//             '<h1 class="has-text-left" style="height: 20px;">'+ '<strong>Descripción: </strong>' + description.value + '</h1>'+
-//             '<br>'+
-//             '<h1 class="has-text-left" style="height: 20px;">'+ '<strong>Responsable: </strong>' + user.value + '</h1>'+
-//             '<h1 class="has-text-left" style="height: 20px;">'+ '<strong>Fecha límite: </strong>' + deadline.value + '</h1>'+
-//         '</article>');
-         
-//         let columnToDoTask = document.getElementById("todoTasks");
-//         let columnInProgress = document.getElementById("inProgress");
-//         let columnDoneTask = document.getElementById("doneTask");
-
-//         if(task.state === "todoTasks"){
-//             columnToDoTask.appendChild()
-//         }
-// }
-
-// showAllTasks();
 importData();
